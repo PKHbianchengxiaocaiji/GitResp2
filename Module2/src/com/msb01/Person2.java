6cebce64f88429866606bef2ea42a827968b7ce3
pkh <2443623673@qq.com>
2022-10-27 20:36:32 +0800

    person2中增加了age属性

---
+++ Module2/src/com/msb01/Person2.java
@@ -0,0 +1,11 @@
+package com.msb01;
+
+/**
+ * @Auther: pkh
+ * @Date: 2022/10/27 - 10 - 27 - 20:35
+ * @Description: com.msb01
+ * @version: 1.0
+ */
+public class Person2 {
+    private int age;
+}
