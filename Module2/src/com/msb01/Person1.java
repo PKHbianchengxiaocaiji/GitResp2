141e7d8c0b8dfb394d07cf14efe66d34b0a4f43c
pkh <2443623673@qq.com>
2022-10-27 20:04:38 +0800

    创建了Person1类

---
+++ Module2/src/com/msb01/Person1.java
@@ -0,0 +1,12 @@
+package com.msb01;
+
+/**
+ * @Auther: pkh
+ * @Date: 2022/10/27 - 10 - 27 - 20:02
+ * @Description: com.msb01
+ * @version: 1.0
+ */
+public class Person1 {
+    private int age;
+    private String name;
+}
